1cde5098568cb9480d0e75cd1a7be886f7c015b9
OakLoaf <oak@beaconstudios.org>
2024-09-16 17:55:30 +0100

    Added maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,3 +54,36 @@ tasks {
         }
     }
 }
+
+publishing {
+    repositories {
+        maven {
+            name = "lushReleases"
+            url = uri("https://repo.lushplugins.org/releases")
+            credentials(PasswordCredentials::class)
+            authentication {
+                isAllowInsecureProtocol = true
+                create<BasicAuthentication>("basic")
+            }
+        }
+
+        maven {
+            name = "lushSnapshots"
+            url = uri("https://repo.lushplugins.org/snapshots")
+            credentials(PasswordCredentials::class)
+            authentication {
+                isAllowInsecureProtocol = true
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = rootProject.group.toString()
+            artifactId = rootProject.name
+            version = rootProject.version.toString()
+            from(project.components["java"])
+        }
+    }
+}
