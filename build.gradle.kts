201c11f33baa831fecb36590b26646d324157438
OakLoaf <oak@beaconstudios.org>
2024-09-17 15:46:37 +0100

    Updated PluginUpdater to 0.3.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
     compileOnly("me.clip:placeholderapi:2.11.2")
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.9")
     implementation("org.enchantedskies:EnchantedStorage:3.0.0")
-    implementation("org.lushplugins.pluginupdater:PluginUpdater-API:0.3.7")
+    implementation("org.lushplugins.pluginupdater:PluginUpdater-API:0.3.10")
     implementation("org.lushplugins:LushLib:0.8.4.2")
 }
 
