b64ae96dfa3e10a9ed0a52e9ef6f2d2eb6d6c6b3
OakLoaf <oak@beaconstudios.org>
2024-10-16 14:57:23 +0100

    Updated metadata API

--- src/main/java/org/lushplugins/pvptoggle/api/PvPToggleAPI.java
+++ src/main/java/org/lushplugins/pvptoggle/api/PvPToggleAPI.java
@@ -1,14 +1,21 @@
 package org.lushplugins.pvptoggle.api;
 
 import org.bukkit.entity.Player;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.Metadatable;
 import org.jetbrains.annotations.NotNull;
 import org.lushplugins.pvptoggle.PvPToggle;
 
 @SuppressWarnings("unused")
 public class PvPToggleAPI {
-    public static final String METADATA_KEY = "RESPECT_PVP_TOGGLE";
+    private static final String METADATA_KEY = "RESPECT_PVP_TOGGLE";
+    private static final FixedMetadataValue METADATA_VALUE = new FixedMetadataValue(PvPToggle.getInstance(), null);
     
     public static boolean isPvPEnabled(@NotNull Player player) {
         return PvPToggle.getInstance().getDataManager().getPvPUser(player).isPvPEnabled();
+    }
+
+    public static void respectPvPToggle(@NotNull Metadatable metadatable) {
+        metadatable.setMetadata(METADATA_KEY, METADATA_VALUE);
     }
 }
