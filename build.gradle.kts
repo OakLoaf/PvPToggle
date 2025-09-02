fb12efd02546bdafac7e6b23da9ba499b792abda
OakLoaf <oak@beaconstudios.org>
2025-09-02 19:29:44 +0100

    Added run-paper and modrinth hook

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,20 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 plugins {
     `java-library`
     `maven-publish`
     id("com.gradleup.shadow") version("8.3.0")
+    id("xyz.jpenilla.run-paper") version("2.2.4")
+    id("com.modrinth.minotaur") version ("2.+")
 }
 
 group = "org.lushplugins"
 version = "2.0.0"
 
 repositories {
-    mavenCentral()
     mavenLocal()
+    mavenCentral()
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://oss.sonatype.org/content/groups/public/")
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/") // PlaceholderAPI
@@ -37,12 +42,17 @@ dependencies {
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 
+    registerFeature("optional") {
+        usingSourceSet(sourceSets["main"])
+    }
+
     withSourcesJar()
 }
 
 tasks {
     withType<JavaCompile> {
         options.encoding = "UTF-8"
+        options.compilerArgs.add("-parameters")
     }
 
     shadowJar {
@@ -53,20 +63,25 @@ tasks {
 
         minimize()
 
-        val folder = System.getenv("pluginFolder")
-        if (folder != null) {
-            destinationDirectory.set(file(folder))
-        }
-
         archiveFileName.set("${project.name}-${project.version}.jar")
     }
 
     processResources{
-        expand(project.properties)
+        filesMatching("plugin.yml") {
+            expand(project.properties)
+        }
 
         inputs.property("version", rootProject.version)
         filesMatching("plugin.yml") {
             expand("version" to rootProject.version)
+        }
+    }
+
+    runServer {
+        minecraftVersion("1.21.7")
+
+        downloadPlugins {
+            hangar("PlaceholderAPI", "2.11.6")
         }
     }
 }
@@ -102,4 +117,51 @@ publishing {
             from(project.components["java"])
         }
     }
+}
+
+modrinth {
+    token.set(System.getenv("MODRINTH_TOKEN"))
+    projectId.set("82jcLX4b")
+    if (System.getenv("RELEASE_TYPE") == "release") {
+        versionNumber.set(rootProject.version.toString())
+        changelog.set(getChangelogSinceLastTag())
+    } else {
+        versionNumber.set("${rootProject.version}-${getCurrentCommitHash()}")
+    }
+    uploadFile.set(file("build/libs/${project.name}-${project.version}.jar"))
+    versionType.set(System.getenv("RELEASE_TYPE"))
+    gameVersions.addAll(
+        "1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.20.5", "1.20.6",
+        "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4", "1.21.5", "1.21.6", "1.21.7", "1.21.8"
+    )
+    loaders.addAll("spigot", "paper", "purpur")
+//    syncBodyFrom.set(rootProject.file("README.md").readText()) // TODO: Add README
+}
+
+tasks.modrinth {
+    dependsOn("shadowJar")
+    dependsOn(tasks.modrinthSyncBody)
+}
+
+fun getCurrentCommitHash(): String {
+    val process = ProcessBuilder("git", "rev-parse", "--short", "HEAD").start()
+    val reader = BufferedReader(InputStreamReader(process.inputStream))
+    val commitHash = reader.readLine()
+    reader.close()
+    process.waitFor()
+    if (process.exitValue() == 0) {
+        return commitHash ?: ""
+    } else {
+        throw IllegalStateException("Failed to retrieve the commit hash.")
+    }
+}
+
+fun getLastTag(): String {
+    return ProcessBuilder("git", "describe", "--tags", "--abbrev=0")
+        .start().inputStream.bufferedReader().readText().trim()
+}
+
+fun getChangelogSinceLastTag(): String {
+    return ProcessBuilder("git", "log", "${getLastTag()}..HEAD", "--pretty=format:* %s ([#%h](https://github.com/OakLoaf/${rootProject.name}/commit/%H))")
+        .start().inputStream.bufferedReader().readText().trim()
 }
